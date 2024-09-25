4c9a71b20b8448b2783def7529a1608d8afce175
Jasurbek <jasurdev1604@gmail.com>
2024-09-25 12:35:57 +0500

    mern

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+require("dotenv").config();
+const workoutRoutes = require("./routes/workouts");
+const mongoose = require("mongoose");
+
+//express app
+const app = express();
+
+//middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//routes
+app.use("/api/workouts", workoutRoutes);
+
+//conntect to db
+
+mongoose
+  .connect(process.env.MONGO_URI, {})
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db and listening on port", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
